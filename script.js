88167130567d47152f2bcc3929f7f64102d6df21
michal <michalwagh@gmail.com>
2022-08-23 17:53:03 +0100

    change element name

--- script.js
+++ script.js
@@ -8,22 +8,18 @@ const cardContainermain = document.querySelector(".card-container");
 
 const showCard = () => {
 
-
-
-    console.log(pokemonArray)
-
-    pokemonArray.forEach(card => {
-        let capitalFirstLeter = card.name.charAt(0).toUpperCase() + card.name.slice(1);
+    pokemonArray.forEach(pokemon => {
+        let capitalFirstLeter = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
 
         const singleCard = document.createElement("div");
         singleCard.classList.add('card');
-        singleCard.innerHTML = `<img class="card__image" src=${card.sprite}>`
+        singleCard.innerHTML = `<img class="card__image" src=${pokemon.sprite}>`
 
         const cardContent = document.createElement("div");
         cardContent.classList.add('card__content');
 
         cardContent.innerHTML = `<h5 class = "card__heading"> ${capitalFirstLeter} </h5> 
-            <p class = "card__text">${capitalFirstLeter} (#${card.id}) is a ${card.types } type pokemon.</p>`
+            <p class = "card__text">${capitalFirstLeter} (#${pokemon.id}) is a ${pokemon.types } type pokemon.</p>`
 
 
         cardContainermain.appendChild(singleCard);
