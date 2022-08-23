79230a6a9e8e3843a8d56c4b5f4c2a95ddc636d2
michal <michalwagh@gmail.com>
2022-08-23 16:13:28 +0100

    description added

--- script.js
+++ script.js
@@ -2,14 +2,21 @@ import {
     pokemonArray
 } from './data/pokemon.js'
 
-const cardContainer = document.querySelector(".card-container");
+const cardContainermain = document.querySelector(".card-container");
 
 
 const showCard = () => {
     pokemonArray.forEach(card => {
-        const cardContainer = document.createElement("div");
-        cardContainer.classList.add('card-container')
-
-
+        const singleCard = document.createElement("div");
+        singleCard.classList.add('card');
+        singleCard.innerHTML = `<img class="card__image" src=${card.sprite}>`
+        const cardContent = document.createElement("div");
+        singleCard.classList.add('card__content')
+        cardContent.innerHTML = `<h5 class = "card__heading"> ${card.name} </h5> 
+            <p class = "card__text">${card.name} (#${card.id}) is a ${card.types } type pokemon.</p>`
+        singleCard.appendChild(cardContent)
+        cardContainermain.appendChild(singleCard);
     });
 }
+
+showCard()
