5b93fcda35b22ad9fa70944de1b6e746cef7d160
michal <michalwagh@gmail.com>
2022-08-23 16:44:32 +0100

    trying capital

--- script.js
+++ script.js
@@ -5,17 +5,31 @@ import {
 const cardContainermain = document.querySelector(".card-container");
 
 
+
 const showCard = () => {
+    // pokemonArray = pokemonArray.map(object => {
+    //     let str = object.name
+    //     str = str.charAt(0).toUpperCase() + str.slice(1);
+    //     console.log(str)
+    //     return str;
+    // })
+
+    console.log(pokemonArray)
+
     pokemonArray.forEach(card => {
         const singleCard = document.createElement("div");
         singleCard.classList.add('card');
         singleCard.innerHTML = `<img class="card__image" src=${card.sprite}>`
+
         const cardContent = document.createElement("div");
-        singleCard.classList.add('card__content')
+        cardContent.classList.add('card__content');
+
         cardContent.innerHTML = `<h5 class = "card__heading"> ${card.name} </h5> 
-            <p class = "card__text">${card.name} (#${card.id}) is a ${card.types } type pokemon.</p>`
-        singleCard.appendChild(cardContent)
+            <p class = "card__text">${(card.name)} (#${card.id}) is a ${card.types } type pokemon.</p>`
+
+
         cardContainermain.appendChild(singleCard);
+        singleCard.appendChild(cardContent);
     });
 }
 
